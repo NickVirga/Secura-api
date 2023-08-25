ffe496eeee010bfa1fcf9bd7960325f43c23b843
Nick Virga <nsvirga@gmail.com>
2023-08-25 09:11:44 -0400

    removed unneeded code from index.js

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ app.use(
 app.use(express.json());
 
 const openai = new OpenAI({
-  // organization: "org-sOGHH8iC9iqsGla6JTxx8EbT",
   apiKey: process.env.OPEN_AI_KEY,
 });
 
@@ -28,7 +27,7 @@ app.post("/ask", async (req, res) => {
     const completion = await openai.completions.create({
       model: "text-davinci-003",
       prompt: prompt,
-      max_tokens: 30,
+      max_tokens: 200,
     });
     const response = completion.choices[0].text;
     return res.status(200).json({
